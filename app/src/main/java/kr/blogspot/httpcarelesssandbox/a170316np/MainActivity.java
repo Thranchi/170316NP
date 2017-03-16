4411c8daa23566d3e823580d95a1eb08a5e347f7
Thranchi <hudin121@gmail.com>
2017-03-16 16:37:54 +0900

    1번완성 2번시작

--- app/src/main/java/kr/blogspot/httpcarelesssandbox/a170316np/MainActivity.java
+++ app/src/main/java/kr/blogspot/httpcarelesssandbox/a170316np/MainActivity.java
@@ -1,13 +1,36 @@
 package kr.blogspot.httpcarelesssandbox.a170316np;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button b1,b2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setTitle("170316 HW");
+
+        b1=(Button)findViewById(R.id.button3);
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,booking.class);
+                startActivity(intent);
+            }
+        });
+        b2=(Button)findViewById(R.id.button4);
+        b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,abcdf.class);
+                startActivity(intent);
+            }
+        });
     }
 }
