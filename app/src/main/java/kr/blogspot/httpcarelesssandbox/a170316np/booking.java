2417c0b29b31532e41b7a08d8b581a80c0f93bdc
Thranchi <hudin121@gmail.com>
2017-03-22 23:05:30 +0900

    2번중

--- app/src/main/java/kr/blogspot/httpcarelesssandbox/a170316np/booking.java
+++ app/src/main/java/kr/blogspot/httpcarelesssandbox/a170316np/booking.java
@@ -1,6 +1,8 @@
 package kr.blogspot.httpcarelesssandbox.a170316np;
 
+import android.os.Build;
 import android.os.SystemClock;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -16,23 +18,26 @@ import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.TimePicker;
 
-public class booking extends AppCompatActivity {
 
+
+public class booking extends AppCompatActivity {
+    int presentpage=0;
+    @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_booking);
         setTitle("Reservation");
 
-        int presentpage=0;
+
 
         Button next,prev;
-        GridLayout people,result;
+        final GridLayout people,result;
         TextView timert,datet,timet,adultt,teent,kidt;
         Switch st;
         FrameLayout ground;
         final DatePicker datep;
-        TimePicker timep;
+        final TimePicker timep;
         LinearLayout buttonbox;
         EditText adultn, teenn, kidn;
         Chronometer timer;
@@ -50,6 +55,10 @@ public class booking extends AppCompatActivity {
         kidt=(TextView)findViewById(R.id.kidt);
         timert=(TextView)findViewById(R.id.timert);
 
+        adultn=(EditText)findViewById(R.id.adultn);
+        teenn=(EditText)findViewById(R.id.teenn);
+        kidn=(EditText)findViewById(R.id.kidn);
+
         timer=(Chronometer)findViewById(R.id.timer);
 
         st=(Switch)findViewById(R.id.startingswitch);
@@ -62,29 +71,113 @@ public class booking extends AppCompatActivity {
 
         buttonbox=(LinearLayout)findViewById(R.id.buttonbox);
 
-        if(st.isChecked())
-        {
-            presentpage=1;
+        if(st.isChecked()) {
+            presentpage = 1;
+            timer.setVisibility(View.VISIBLE);
+            timet.setVisibility(View.VISIBLE);
             timer.start();
-                    prev.setEnabled(false);
-                    datep.setVisibility(View.VISIBLE);
+            for (; ; ) {
+                switch (presentpage) {
+                    case 1: {
+                        datep.setVisibility(View.VISIBLE);
+                        prev.setEnabled(false);
+                        next.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                presentpage += 1;
+                                datep.setVisibility(View.GONE);
+                            }
+                        });
+                        break;
+                    }
+                    case 2:{
+                        timep.setVisibility(View.VISIBLE);
+                        next.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                presentpage += 1;
+                                timep.setVisibility(View.GONE);
+                            }
+                        });
+                        prev.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                presentpage -= 1;
+                                timep.setVisibility(View.GONE);
 
-                    next.setOnClickListener(new View.OnClickListener() {
-                        @Override
-                        public void onClick(View v) {
-                            datep.setVisibility(View.GONE);
+                            }
+                        });
+                        break;
+                    }
 
+                    case 3:{
+                        people.setVisibility(View.VISIBLE);
+                        next.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                presentpage += 1;
+                                people.setVisibility(View.GONE);
+                            }
+                        });
+                        prev.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                presentpage -= 1;
+                                people.setVisibility(View.GONE);
+
+                            }
+                        });
+                        break;
+                    }
+
+                    case 4:{
+                        result.setVisibility(View.VISIBLE);
+                        timer.stop();
+                        datet.setText(datep.getYear()+"."+datep.getMonth()+"."+datep.getDayOfMonth()+".");
+                        timet.setText(timep.getHour()+" : "+timep.getMinute());
+                        String ad = adultn.getText().toString();
+                        if(ad.getBytes().length <= 0)
+                        {
+                            ad="0";
                         }
-                    });
-                            timer.stop();
-            next.setEnabled(false);
+                        String te = teenn.getText().toString();
+                        if(ad.getBytes().length <= 0)
+                        {
+                            te="0";
+                        }
+                        String ki = kidn.getText().toString();
+                        if(ad.getBytes().length <= 0)
+                        {
+                            ki="0";
+                        }
+                        adultt.setText(ad+" .");
+                        teent.setText(ad+" .");
+                        kidt.setText(ad+" .");
+
+                        next.setEnabled(false);
+                        prev.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                presentpage -= 1;
+                                result.setVisibility(View.GONE);
+                            }
+                        });
+                        break;
+                    }
+
+
+
+
+                }
+            }
         }
         else
         {
             presentpage=0;
 
             timer.setBase(SystemClock.elapsedRealtime());
-
+            timer.setVisibility(View.GONE);
+            timert.setVisibility(View.GONE);
             datet.setText(null);
             timet.setText(null);
             adultt.setText(null);
@@ -92,6 +185,7 @@ public class booking extends AppCompatActivity {
             kidt.setText(null);
 
             buttonbox.setVisibility(View.GONE);
+            people.setVisibility(View.GONE);
             ground.setVisibility(View.GONE);
             datep.setVisibility(View.GONE);
             timep.setVisibility(View.GONE);
